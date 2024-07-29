30adeee75d4acdec00e58aa2084dd700dcd4f87e
npoudelp <npoudelp@gmail.com>
2024-07-29 17:34:44 +0545

    image compression enhanced

--- bca/utils/models.py
+++ bca/utils/models.py
@@ -75,7 +75,7 @@ class AboutBca(models.Model):
 def compress(image):
     im = Image.open(image)
     im_io = BytesIO() 
-    im.save(im_io, 'JPEG', quality=60) 
+    im.save(im_io, 'JPEG', quality=50) 
     new_image = ContentFile(im_io.getvalue(), image.name)
     return new_image
 
